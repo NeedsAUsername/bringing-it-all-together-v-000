14979dde4d0b3c59c620972eaad12b06b38bdc77
Kevin Chi <kevinchi118@gmail.com>
2018-07-18 01:10:30 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -83,7 +83,7 @@ class Dog
         sql = <<-SQL
             SELECT *
             FROM dogs
-            WHERE name = ?
+            WHERE dogs.name = ?
         SQL
 
         DB[:conn].execute(sql, name).collect do |row|
