b18fe243f6d276f995258e04385212980a37175f
Kevin Chi <kevinchi118@gmail.com>
2018-07-18 01:03:56 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -66,12 +66,12 @@ class Dog
             WHERE name = ?, breed = ?
         SQL
 
-        dog = DB[:conn].execute(sql, name, breed)
+        dog_data = DB[:conn].execute(sql, name, breed)
         if !dog.empty?
             attributes = dog[0]
             dog = self.new(name: attributes[1], breed: attributes[2], id: attributes[0])
         else dog = self.create(name: name, breed: breed)
         end
-        dog 
+        dog
     end
 end
